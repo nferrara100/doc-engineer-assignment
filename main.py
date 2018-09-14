d3ddda38ca49b94fdddda72466451f79002e714a
Nicholas Ferrara <nferrara100@gmail.com>
2018-09-14 20:58:26 +0100

    Basic pushing to Algolia API

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
-# from algoliasearch import algoliasearch
-# from config import Config
+from algoliasearch import algoliasearch
+from config import Config
 from html.parser import HTMLParser
 import os
-
-
-# client = algoliasearch.Client(Config.algolia_key, Config.algolia_secret)
-# # index = client.init_index('doc-test-demo')
 
 
 class DocParser(HTMLParser):
     current_tag = ''
     current_text = ''
-    wanted_tags = ['h1', 'h2', 'h3', 'h4', 'p']
+    wanted_tags = ['h1', 'h2', 'h3', 'h4']
+
+    def __init__(self):
+        self.client = algoliasearch.Client(Config.algolia_key, Config.algolia_secret)
+        self.index = self.client.init_index('doc-test')
+        HTMLParser.__init__(self)
 
     def handle_starttag(self, tag, attrs):
         if tag in self.wanted_tags:
-            print(self.current_tag + " : " + self.current_text)
+            if self.current_text != "":
+                res = self.index.add_object({"tag": "p", "super": self.current_tag, "text": self.current_text})
             self.current_tag = tag.lower()
 
     def handle_endtag(self, tag):
         if tag in self.wanted_tags:
-            pass
+            res = self.index.add_object({"tag": tag, "text": self.current_text})
+            self.current_text = ''
 
     def handle_data(self, data):
         self.current_text += data
@@ -34,4 +37,3 @@ for file in os.walk(folder, followlinks=True):
         if filename[-5:] == ".html":
             with open(filename, 'r') as webpage:
                 parser.feed(webpage.read())
-print("Done")
