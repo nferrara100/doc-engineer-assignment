5da2a9b7ef9ab7aea22b35a5307e8e0f6d19af53
Nicholas Ferrara <nferrara100@gmail.com>
2018-09-14 19:34:07 +0100

    Basic parser working

---
+++ config-example.py
@@ -0,0 +1,7 @@
+# This is an example configuration file. The actual file with the live API details is hidden for security reasons.
+# Get your own API key with a trial of Algolia at algolia.com.
+
+
+class Config:
+    algolia_key = None
+    algolia_secret = None
---
+++ main.py
@@ -0,0 +1,45 @@
+from algoliasearch import algoliasearch
+from config import Config
+from html.parser import HTMLParser
+import os
+
+
+# client = algoliasearch.Client(Config.algolia_key, Config.algolia_secret)
+# # index = client.init_index('doc-test-demo')
+
+
+class DocParser(HTMLParser):
+    current_tag = []
+    current_text = ''
+    wanted_tags = ['h1', 'h2', 'h3', 'h4', 'p']
+
+    def handle_starttag(self, tag, attrs):
+        if tag in self.wanted_tags:
+            self.current_tag.append((tag.lower(), ''))
+
+    def handle_endtag(self, tag):
+        if tag in self.wanted_tags:
+            try:
+                print(tag + " : " + self.current_text)
+                self.current_text = ''
+                self.current_tag.pop()
+            except IndexError:
+                raise IndexError('There is an unknown endtag in the HTML. Remove it and try again.')
+
+    def handle_data(self, data):
+        self.current_text += data
+        # try:
+        #     print(self.current_tag[-1] + " : " + data)
+        # except IndexError:
+        #     pass
+        #    # print("The following text is not nested in the HTML and was not indexed: " + data)
+
+
+parser = DocParser()
+folder = "source"
+for file in os.walk(folder, followlinks=True):
+    for i in range(len(file[2])):
+        filename = file[0] + os.sep + file[2][i]
+        if filename[-5:] == ".html":
+            with open(filename, 'r') as webpage:
+                parser.feed(webpage.read())
