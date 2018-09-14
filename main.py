a339c207db2f2ef5c5a40551b524b9ed3b73b90a
Nicholas Ferrara <nferrara100@gmail.com>
2018-09-14 20:14:55 +0100

    Nested list in parser draft

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from algoliasearch import algoliasearch
-from config import Config
+# from algoliasearch import algoliasearch
+# from config import Config
 from html.parser import HTMLParser
 import os
 
@@ -10,29 +10,28 @@ import os
 
 class DocParser(HTMLParser):
     current_tag = []
-    current_text = ''
     wanted_tags = ['h1', 'h2', 'h3', 'h4', 'p']
 
     def handle_starttag(self, tag, attrs):
         if tag in self.wanted_tags:
-            self.current_tag.append((tag.lower(), ''))
+            self.current_tag.append([tag.lower(), ''])
 
     def handle_endtag(self, tag):
         if tag in self.wanted_tags:
             try:
-                print(tag + " : " + self.current_text)
-                self.current_text = ''
+                if tag == self.current_tag[-1:][0]:
+                    print(tag + " : " + self.current_tag[-1:][1])
                 self.current_tag.pop()
             except IndexError:
+                # pass
                 raise IndexError('There is an unknown endtag in the HTML. Remove it and try again.')
 
     def handle_data(self, data):
-        self.current_text += data
-        # try:
-        #     print(self.current_tag[-1] + " : " + data)
-        # except IndexError:
-        #     pass
-        #    # print("The following text is not nested in the HTML and was not indexed: " + data)
+        try:
+            self.current_tag[-1:][1] += data
+        except IndexError:
+            pass
+            # print("The following text is not nested in the HTML and was not indexed: " + data)
 
 
 parser = DocParser()
@@ -43,3 +42,4 @@ for file in os.walk(folder, followlinks=True):
         if filename[-5:] == ".html":
             with open(filename, 'r') as webpage:
                 parser.feed(webpage.read())
+print("Done")
