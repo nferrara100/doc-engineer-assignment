4048a9dd2fb7f7ac7af4a0f9b63ecd56ba81aa7e
Nicholas Ferrara <nferrara100@gmail.com>
2018-09-16 22:35:08 +0100

    Better formatting on front end

--- main.js
+++ main.js
@@ -1,40 +1,38 @@
 const search = instantsearch({
-    appId: 'P1CNOY80LV',
-    apiKey: '12a0e2f2e0c7c22ef8220bb905900d10',
-    indexName: 'doc-test',
-    routing: true
+	appId: 'P1CNOY80LV',
+	apiKey: '12a0e2f2e0c7c22ef8220bb905900d10',
+	indexName: 'doc-test',
+	routing: true
 });
 
-  // initialize SearchBox
-  search.addWidget(
-    instantsearch.widgets.searchBox({
-      container: '#search-box',
-      placeholder: 'Search the docs'
-    })
-  );
+// initialize SearchBox
+search.addWidget(
+	instantsearch.widgets.searchBox({
+		container: '#search-box',
+		placeholder: 'Search the docs'
+	})
+);
 
-  const hitTemplate = '<a href="{{{link}}}">{{{h1}}}' +
-      '</a><br/>{{{h2}}}<br/><br/>'
+const hitTemplate = '<a href="{{{link}}}">{{{h1}}}</a><br/>{{{h2}}}<br/><br/>'
 
-
-  search.addWidget(
-    instantsearch.widgets.hits({
-      container: '#hits',
-      templates: {
-        empty: 'No results',
-        item: hitTemplate
-      }
-    })
-  );
+search.addWidget(
+	instantsearch.widgets.hits({
+		container: '#hits',
+		templates: {
+			empty: 'No results',
+			item: hitTemplate
+		}
+	})
+);
 
 // initialize pagination
 search.addWidget(
-instantsearch.widgets.pagination({
-  container: '#pagination',
-  maxPages: 20,
-  // default is to scroll to 'body', here we disable this behavior
-  scrollTo: false
-})
+	instantsearch.widgets.pagination({
+		container: '#pagination',
+		maxPages: 20,
+		// default is to scroll to 'body', here we disable this behavior
+		scrollTo: false
+	})
 );
 
 search.start();
