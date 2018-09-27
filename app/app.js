0e5ab63aab7a6548b285f2eba640e84bc41ea936
Nicholas Ferrara <nferrara100@gmail.com>
2018-09-27 03:22:23 +0100

    Fully commented

--- app/app.js
+++ app/app.js
@@ -1,7 +1,34 @@
+// Format each result from a search
+function formatHit(hit) {
+    const tags = ['h1', 'h2', 'h3', 'h4', 'content'];
+    var formatted = '<a href="' + hit.link + '#' + hit.hash + '">';
+    var linked = false;
+    for(let i = 0; i < 4; i++){
+        // Do this for each of the heading tags
+        if (hit[tags[i]]) {
+            formatted += hit._highlightResult[tags[i]].value;
+            // Close the <a> tag after the first hit
+            if (!linked) {
+                formatted += '</a><br /> ';
+                linked = true;
+            }
+            else {
+                formatted += ": "
+            }
+        }
+    }
+    // Add all of the content of the page
+    if (hit.content) {
+        formatted += hit._snippetResult.content.value
+    }
+    return formatted + '<br /><br />';
+}
+
+// Set up the Instant Search API
 const search = instantsearch({
 	appId: 'P1CNOY80LV',
 	apiKey: '12a0e2f2e0c7c22ef8220bb905900d10',
-	indexName: 'doc-test',
+	indexName: 'doc-ex',
     urlSync: true
 });
 
@@ -13,28 +40,7 @@ search.addWidget(
 	})
 );
 
-function formatHit(hit) {
-    const tags = ['h1', 'h2', 'h3', 'h4', 'content'];
-    var formatted = '<a href="' + hit.link + '#' + hit.hash + '">';
-    var linked = false;
-    for(let i = 0; i < 4; i++){
-        if (hit[tags[i]]) {
-            formatted += hit._highlightResult[tags[i]].value;
-            if (!linked) {
-                formatted += '</a><br /> ';
-                linked = true;
-            }
-            else {
-                formatted += ": "
-            }
-        }
-    }
-    if (hit.content) {
-        formatted += hit._snippetResult.content.value
-    }
-    return formatted + '<br /><br />';
-}
-
+// Displays the result of the search
 search.addWidget(
 	instantsearch.widgets.hits({
 		container: '#hits',
--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ import time
 class Algolia:
     def __init__(self):
         self.client = algoliasearch.Client(Config.algolia_key, Config.algolia_secret)
-        self.index = self.client.init_index('doc-test')
+        self.index = self.client.init_index('doc-ex')
 
 
 # Take given HTML text and parse into searchable Algolia database
@@ -18,6 +18,7 @@ class DocParser(HTMLParser):
     # Needs an Algolia class and the page in a string format to proceed.
     def __init__(self, algolia, page):
         self.algolia = algolia
+        # The replace call ensures compatibility with Windows
         self.page = page.replace(os.sep, '/')
         self.location = ''
         self.unwanted = False
@@ -26,26 +27,31 @@ class DocParser(HTMLParser):
         self.current_tag = ''
         self.current_text = ''
         self.wanted_tags = ['h1', 'h2', 'h3', 'h4']
+        # The inner contents of these tags should not be displayed and are thus not recorded
         self.unwanted_tags = ['audio', 'canvas', 'map', 'meta', 'object', 'script', 'source', 'style', 'video']
         self.total_records = 0
         HTMLParser.__init__(self)
 
     # Every time a start tag is encountered in the document save it in case it contains something useful
     def handle_starttag(self, tag, attrs):
+        # See if this tag should be recorded separately
         if tag in self.wanted_tags:
             self.save_record(self.current_text.strip())
             self.unwanted = False
             self.current_tag = tag
             self.location = ''
+            # The lower the importance number the more important it is
             self.importance = self.wanted_tags.index(self.current_tag)
+        # If a location isn't saved use this link as a hash bookmark
         elif tag == 'a' and self.location == '':
             for name, value in attrs:
                 if name == 'id':
                     self.location = value
+        # This prevents data from being recorded that is not meant to be human readable
         elif tag in self.unwanted_tags:
             self.unwanted = True
 
-    # Once an element is completed save it if it was useful
+    # Once an element is completed save it if it was useful. Let it become part of the next record if it was not.
     def handle_endtag(self, tag):
         if tag in self.wanted_tags:
             self.unwanted = False
@@ -57,10 +63,11 @@ class DocParser(HTMLParser):
         if self.unwanted is False:
             self.current_text += data
 
-    # Save the collected data so far in the class to Algolia and then wipe that data locally
+    # Save the collected data so far in the class to Algolia and then wipe that data locally so it is not added again
     def save_record(self, content=''):
         if self.current_text.strip() != "":
             record = {}
+            # Save each of the heading tags in their own field
             for i in range(self.importance + 1):
                 if self.headings[i] != '':
                     record[self.wanted_tags[i]] = self.headings[i]
